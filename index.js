e2447fe3a1701d5fbb8d985a395fa23d17a137b5
Rafael Huijon <rhuijonp@gmail.com>
2018-02-12 18:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return "You are number" + katzDeliLine.length + " in line.";
 }
+
+function nowServing() {
+  var nowServing = "";
+  if (katzDeliLine.length > 0) {
+    nowServing = katzDeliLine[0];
+  } else {
+    nowServing = 
+  }
+  
+  
+}
